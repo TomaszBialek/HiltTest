8150c6ef259fa44489cc188b82ea040faa8525e2
Tomasz Białek <bialek.tomasz225@gmail.com>
2020-07-19 19:34:39 +0200

    Retrofit Gradle + Model

---
+++ app/src/main/java/com/example/hilttest/retrofit/NetworkMapper.kt
@@ -0,0 +1,30 @@
+package com.example.hilttest.retrofit
+
+import com.example.hilttest.model.Blog
+import com.example.hilttest.util.EntityMapper
+import javax.inject.Inject
+
+class NetworkMapper @Inject constructor() : EntityMapper<BlogNetworkEntity, Blog> {
+
+    override fun mapFromEntity(entity: BlogNetworkEntity): Blog {
+        return Blog(
+            id = entity.id,
+            title = entity.title,
+            body = entity.body,
+            image = entity.image,
+            category = entity.category
+        )
+    }
+
+    override fun mapToEntity(domainModel: Blog): BlogNetworkEntity {
+        return BlogNetworkEntity(
+            id = domainModel.id,
+            title = domainModel.title,
+            body = domainModel.body,
+            image = domainModel.image,
+            category = domainModel.category
+        )
+    }
+
+    fun mapFromEntityList(entities: List<BlogNetworkEntity>) = entities.map { mapFromEntity(it) }
+}
